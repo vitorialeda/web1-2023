05d9269e41bc992245ae941082ccb17920d52101
Vitória <vitoria_lnogueira@hotmail.com>
2025-06-03 08:12:01 -0300

    funcao de formatacao do valor

--- projeto-reembolso/script.js
+++ projeto-reembolso/script.js
@@ -1,16 +1,22 @@
+// Referencia componente que guardará os itens
 const expensesList = document.querySelector("ul");
 
 
 document.querySelector("form").addEventListener("submit", (e) => {
+    // Evita o redirecionamento
     e.preventDefault();
 
+    // Guarda os valores do formulário em variáveis
     const expenseName = document.getElementById("expense").value;
     const expenseCategory = document.getElementById("category").value;
     const expenseValue = document.getElementById("amount").value;
 
+    // Novo item da lista que conterá os valores do formulário
     const newItem = document.createElement("li");
+    // Adiciona classe ao item para estilização
     newItem.classList.add("expense");
 
+    // Conteúdo interno do item
     newItem.innerHTML = `
             <img src="./img/food.svg" alt="Ícone de tipo da despesa" />
 
@@ -19,15 +25,20 @@ document.querySelector("form").addEventListener("submit", (e) => {
               <span>${expenseCategory}</span>
             </div>
 
-            <span class="expense-amount"><small>R$</small>${expenseValue}</span>`;
+            <span class="expense-amount"><small>R$</small>${formatValue(expenseValue)}</span>`;
 
+
+    // Novo botão de exclusão já com a função de deletar item
     const deleteButton = createDeleteButton();
+    // Adiciona botão ao item
     newItem.appendChild(deleteButton);
 
+    // Adiciona novo item à ul
     expensesList.appendChild(newItem);
 });
 
 
+// Cria botão p/ exclusão de item
 function createDeleteButton() {
     const deleteButton = document.createElement("img");
     deleteButton.setAttribute("src", "./img/remove.svg");
@@ -38,7 +49,13 @@ function createDeleteButton() {
     return deleteButton;
 }
 
-
+// Função de deletar item
 function deleteItem() {
+    // Remove o elemento pai do botão
     this.parentNode.remove();
+}
+
+// Adiciona duas casas decimais ao numero
+function formatValue(value) {
+    return Number(value).toLocaleString("pt-br", { minimumFractionDigits: 2 })
 }
