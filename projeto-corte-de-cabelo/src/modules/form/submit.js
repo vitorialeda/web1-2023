980cc0337adece19a0aed2f2b3f276ca0ff954f7
Vitória Leda <vitoria_lnogueira@hotmail.com>
2025-06-25 14:27:08 -0300

    init projeto corte de cabelo

---
+++ projeto-corte-de-cabelo/src/modules/form/submit.js
@@ -0,0 +1,52 @@
+import dayjs from "dayjs"
+
+import {scheduleNew} from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js"
+
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
+
+//Data atual para o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+//Carrega a data atual e define a data mínima como sendo a data atual.
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = async (event) => {
+    //Previne o comportamento padrão de carregar a página.
+    event.preventDefault()
+
+    try {
+        //Recuperando o nome do cliente.
+        const name = clientName.value.trim()
+        if(!name){
+            return alert("Informe o nome do cliente!")
+        }
+        //Recupera o horário selecionado.
+        const hourSelected = document.querySelector(".hour-selected")
+        if(!hourSelected){
+            return alert("Selecione a hora.")
+        }
+        //Recupera somente a hora.
+        const [hour] = hourSelected.innerText.split(":")
+        //Insere a hora na data.
+        const when = dayjs(selectedDate.value).add(hour,"hour")
+        //Gera um ID.
+        const id = new Date().getTime().toString()
+        //Faz o agendamento.
+        await scheduleNew({
+            id,
+            name,
+            when
+        })
+        //Recarrega os agendamentos.
+        await schedulesDay()
+        //Limpa o input de nome do cliente.
+        clientName.value = ""
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
+}
---
+++ projeto-corte-de-cabelo/src/modules/schedules/load.js
@@ -0,0 +1,20 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import { hoursLoad } from "../form/hours-load.js"
+import { schedulesShow } from "./show.js"
+
+// Seleciona o input de data.
+const selectedDate = document.getElementById("date")
+
+export async function schedulesDay() {
+    // Obtém a data do input
+    const date = selectedDate.value
+
+    //Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date})
+
+    //Exibe os agendamentos
+    schedulesShow({dailySchedules})
+
+    // Renderiza as horas disponíveis.
+    hoursLoad({date, dailySchedules})
+}
